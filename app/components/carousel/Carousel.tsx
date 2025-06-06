d7ed754c7bb26c10d04342cf738ece1b06a927d5
ola-123456789 <73062218+ola-123456789@users.noreply.github.com>
2025-06-06 23:22:32 +0400

    text on carousel

--- app/components/carousel/Carousel.tsx
+++ app/components/carousel/Carousel.tsx
@@ -19,18 +19,34 @@ const contentStyle: React.CSSProperties = {
 
 const App: React.FC = () => (
     <div className={styles.carouselDiv}>
-        <Carousel autoplay arrows effect="fade" className={`${styles.carousel}`}>
+        <Carousel arrows effect="fade" className={`${styles.carousel}`}>
             <div>
                 <img src="/slide 1.webp" alt="" />
+                <div className={styles.CarouselTextDiv}>
+                    <h5>The best Designer 1 is:</h5>
+                    <p>Ketevan shatberashvili 1</p>
+                </div>
             </div>
             <div>
                 <img src="/slide 2.jpeg" alt="" />
+                <div className={styles.CarouselTextDiv}>
+                    <h5>The best Designer 2 is:</h5>
+                    <p>Ketevan shatberashvili 2</p>
+                </div>
             </div>
             <div>
                 <img src="/slide 3.jpg" alt="" />
+                <div className={styles.CarouselTextDiv}>
+                    <h5>The best Designer 3 is:</h5>
+                    <p>Ketevan shatberashvili 3</p>
+                </div>
             </div>
             <div>
                 <img src="/slide 4.jpg" alt="" />
+                <div className={styles.CarouselTextDiv}>
+                    <h5>The best Designer 4 is:</h5>
+                    <p>Ketevan shatberashvili 4</p>
+                </div>
             </div>
         </Carousel>
 
